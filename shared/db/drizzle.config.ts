3746e9b821b2cac8d20cb098ba0d2bdc588b3bcf
n-toxic <d48965846@gmail.com>
2026-05-08 23:52:57 +0530

    Update drizzle.config.ts

--- shared/db/drizzle.config.ts
+++ shared/db/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 import path from "path";
 
-const DB_URL = "postgresql://neondb_owner:npg_nHRXNL0bzoQ1@ep-little-flower-apyhb8d6-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require";
+const DB_URL = "postgres://avnadmin:AVNS_AD9kGq6Hy2cuvWnDmcl@toxin-d48965846-abf6.b.aivencloud.com:22607/defaultdb";
 
 export default defineConfig({
   schema: "./src/schema/index.ts",
