b5133ce353dbf5cbfcc3719bc23bb82a8450e930
n-toxic <d48965846@gmail.com>
2026-05-08 16:55:22 +0530

    Update logger import to include .js extension

--- backend/src/lib/mailer.ts
+++ backend/src/lib/mailer.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { logger } from "./logger";
+import { logger } from "./logger.js";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
