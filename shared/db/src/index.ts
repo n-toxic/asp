5b43889e91a6a7a1cfe4f03d6a35a2d31824b381
n-toxic <d48965846@gmail.com>
2026-05-08 17:14:20 +0530

    Update index.ts

--- shared/db/src/index.ts
+++ shared/db/src/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-export * from "./schema/index.js"; 
+import * as schema from "./schema/index.js"; 
 
 const Pool = pg.Pool;
 
@@ -11,12 +11,10 @@ if (!connectionString) {
 
 export const pool = new Pool({ 
   connectionString,
-  // Vercel/Railway par connection pooling ke liye ye helpful hota hai
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
 
 export const db = drizzle(pool, { schema });
-
-export * from "./schema.js";
+export * from "./schema/index.js";
